9c01badc0af2984010bf5fde87343f78ba41f4fd
VedatDaglar <vedatdaglar@protonmail.com>
2022-10-14 15:43:33 +0200

    Initial commit

---
+++ src/test/java/com/nextbasecrm/runners/CukesRunner.java
@@ -0,0 +1,26 @@
+package com.nextbasecrm.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                //"pretty",
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,  // browser will open when false
+        tags = "@scientists",
+        publish = true
+)
+public class CukesRunner {
+}
