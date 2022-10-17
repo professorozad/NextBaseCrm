6538c1afcb456d00ef428019bd73dd6df71e7c25
VedatDaglar <vedatdaglar@protonmail.com>
2022-10-17 02:13:09 +0200

    login function

--- src/test/java/com/nextbasecrm/runners/CukesRunner.java
+++ src/test/java/com/nextbasecrm/runners/CukesRunner.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/nextbasecrm/step_definitions",
         dryRun = false,  // browser will open when false
-        tags = "@login",
+        tags = "",
         publish = true
 )
 public class CukesRunner {
